583e413ce443d7ed1f958fbad88cd1589d3a959e
0qiqi0 <1483593015@qq.com>
2016-04-13 20:35:10 +0800

    end

--- socket.io/server.js
+++ socket.io/server.js
@@ -15,15 +15,34 @@ app.get('/',function(req,res){
 var server=require('http').createServer(app);
 //websocket握手需借助http
 var io = require('socket.io')(server);
-var clients=[];
+var clients={};
 io.on('connection',function(socket){
+    var username;
+    socket.send({user:'系统',content:'请输入用户名'});
 //socket代表与某个客户端的连接对象
-    clients.push(socket);
+    //clients.push(socket);
     socket.on('message',function(msg){
-        //socket.send('server:'+msg)
-        clients.forEach(function(client){
-            client.send(msg);
-        })
+        var result=msg.match(/^@(.+)\s(.+)$/); ///s是空格之类的
+        if(result){
+            console.log(result[0],result[1])
+            var toUser=result[1];
+            var content=result[2];//第0个是匹配到的字符串，第1个是第一个分组
+            if(clients[toUser]){
+                clients[toUser].send({user:username,content:'[私聊]'+content});
+            }else{
+                socket.send({user:'系统',content:'你想私聊的人不在线'});
+            }
+        }else{
+            if(username){
+                for(var s in clients){
+                    clients[s].send({user:username,content:msg});
+                }
+            }else{
+                username=msg;
+                clients[username]=socket;
+                socket.send({user:'系统',content:'你的用户名已改为:'+username});
+            }
+        }
     })
 });
 server.listen(80);
